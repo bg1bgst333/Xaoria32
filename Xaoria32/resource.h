185d327e987c73a19368d1e19e015329d944c92f
bg1bgst333 <bg1bgst333@gmail.com>
2018-05-01 13:39:27 +0900

    -マップをリソースからインポート.

--- Xaoria32/resource.h
+++ Xaoria32/resource.h
@@ -13,13 +13,15 @@
 #define IDB_SHARED3                     106
 #define IDB_SHARED1                     107
 #define IDB_SHARED2                     108
+#define IDR_BIN1                        109
+#define IDR_TESTMAP1                    109
 #define ID_APP_EXIT                     40001
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        109
+#define _APS_NEXT_RESOURCE_VALUE        110
 #define _APS_NEXT_COMMAND_VALUE         40002
 #define _APS_NEXT_CONTROL_VALUE         1001
 #define _APS_NEXT_SYMED_VALUE           101
